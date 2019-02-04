785e58e52dd7fc4dfaa8bf0121fc4a29b206e9ef
Tony Roberts <tony@pyxll.com>
2019-02-04 13:28:04 +0000

    Add version number to Bloomberg API DLL name
    On Windows get the version info from the Bloomberg API DLL and create a
    version specific lib/dll for the Python extensions to link against.
    
    This resolves any conflict where Python in embedded somewhere where
    another version of the Bloomberg API is already being loaded (eg Excel).

--- setup.py
+++ setup.py
@@ -31,7 +31,115 @@ if is64bit:
 else:
     blpapiLibraryName = 'blpapi3_32'
 
+cmdclass = {}
 if platform == 'windows':
+    from distutils.command import build_ext
+    from ctypes import windll, create_string_buffer, c_uint, byref, string_at, wstring_at, c_void_p
+    import shutil
+    import array
+    import re
+
+    class BuildBlpExtension(build_ext.build_ext):
+        @staticmethod
+        def get_file_info(filename, info):
+            """
+            Extract information from a file.
+            """
+            if version >= '3.0':
+                GetFileVersionInfoSize = windll.version.GetFileVersionInfoSizeW
+                GetFileVersionInfo =  windll.version.GetFileVersionInfoW
+                VerQueryValue = windll.version.VerQueryValueW
+                tstring_at = wstring_at
+            else:
+                GetFileVersionInfoSize = windll.version.GetFileVersionInfoSizeA
+                GetFileVersionInfo = windll.version.GetFileVersionInfoA
+                VerQueryValue = windll.version.VerQueryValueA
+                tstring_at = string_at
+
+            # Get size needed for buffer (0 if no info)
+            size = GetFileVersionInfoSize(filename, None)
+            if not size:
+                return ''
+
+            # Create buffer
+            res = create_string_buffer(size)
+
+            # Load file informations into buffer res
+            GetFileVersionInfo(filename, None, size, res)
+            r = c_void_p()
+            l = c_uint()
+
+            # Look for codepages
+            VerQueryValue(res, '\\VarFileInfo\\Translation', byref(r), byref(l))
+
+            # If no codepage -> empty string
+            if not l.value:
+                return ''
+
+            # Take the first codepage
+            codepages = array.array('H', string_at(r.value, l.value))
+            codepage = tuple(codepages[:2].tolist())
+
+            # Extract information
+            VerQueryValue(res, ('\\StringFileInfo\\%04x%04x\\' + info) % codepage, byref(r), byref(l))
+            return tstring_at(r.value, l.value-1).replace(" ", "").replace(",", ".")
+
+        def build_extension(self, ext):
+            """
+            Builds the C extension libraries, but replaces blpapi dll dependency with
+            a versioned copy so it doesn't conflict with other Bloomberg tools.
+            """
+            # Replace blpapiLibraryName with the version specific one
+            if blpapiLibraryName in ext.libraries:
+                srcdll = os.path.join(blpapiLibraryPath, blpapiLibraryName + ".dll")
+                version = self.get_file_info(srcdll, "FileVersion")
+                versionedLibName = blpapiLibraryName + "_" + version
+                build_dir = os.path.dirname(self.get_ext_fullpath(ext.name))
+
+                if self.force or not os.path.exists(os.path.join(self.build_temp, versionedLibName + ".lib")):
+                    if not self.compiler.initialized:
+                        self.compiler.initialize()
+
+                    if not os.path.exists(self.build_temp):
+                        os.makedirs(self.build_temp)
+
+                    # dump the def file for the dll to rebuild the lib file
+                    dumpbinfile = os.path.join(self.build_temp, blpapiLibraryName + "_" + version + ".dumpbin")
+                    dumpbin = os.path.join(os.path.dirname(self.compiler.lib), "DUMPBIN.EXE")
+                    self.compiler.spawn([dumpbin, "/EXPORTS", "/OUT:" + dumpbinfile,  srcdll])
+
+                    # get all the function definitions
+                    deffile = os.path.join(self.build_temp, blpapiLibraryName + "_" + version + ".def")
+                    with open(deffile, "wt") as out_fh:
+                        exports = []
+                        for line in open(dumpbinfile).readlines():
+                            matches = re.search(r'^\s*(\d+)\s+[A-Z0-9]+\s+[A-Z0-9]{8}\s+([^ ]+)', line)
+                            if matches:
+                                exports.append(matches.group(2) + "\n")
+                        out_fh.writelines(["EXPORTS\n"] + exports)
+
+                    # rebuild the lib file with the new dll name
+                    libfile = os.path.join(self.build_temp, blpapiLibraryName + "_" + version + ".lib")
+                    machine = "/MACHINE:" + ("X64" if is64bit else "X86")
+                    self.compiler.spawn([self.compiler.lib, machine, "/DEF:" + deffile, "/OUT:" + libfile])
+
+                # copy the versioned dll the the build output
+                if self.force or not os.path.exists(os.path.join(build_dir, versionedLibName + ".dll")):
+                    if not os.path.exists(build_dir):
+                        os.makedirs(build_dir)
+                    shutil.copy(os.path.join(blpapiLibraryPath, blpapiLibraryName + ".dll"),
+                                os.path.join(build_dir, versionedLibName + ".dll"))
+
+                # replace blpapi.lib with the versioned one
+                ext.libraries = [versionedLibName if x == blpapiLibraryName else x for x in ext.libraries]
+                ext.library_dirs.insert(0, self.build_temp)
+
+            build_ext.build_ext.build_extension(self, ext)
+
+    cmdclass.update({
+         "build_ext": BuildBlpExtension,
+    })
+
     blpapiLibraryPath = os.path.join(blpapiRoot, 'lib')
     extraLinkArgs = ['/MANIFEST']
 
@@ -49,6 +157,8 @@ elif platform == 'darwin':
     extraLinkArgs = []
 else:
     raise Exception("Platform '" + platform + "' isn't supported")
+
+
 
 blpapiLibraryPath = blpapiLibVar or blpapiLibraryPath
 blpapiIncludes = blpapiIncludesVar or os.path.join(blpapiRoot, 'include')
@@ -80,6 +190,7 @@ setup(
     ext_modules=[blpapi_wrap, versionhelper_wrap],
     url='http://www.bloomberglabs.com/api/',
     packages=["blpapi"],
+    cmdclass=cmdclass,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
